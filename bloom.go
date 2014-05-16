a940e242f68d3d8d11112fc4268acaea660717db
Xavier Perez <duosrx@gmail.com>
2014-05-16 10:59:32 +0200

    Extract get() and set()

--- bloom.go
+++ bloom.go
@@ -19,18 +19,26 @@ func str_to_fnv(s string) (uint64, uint64) {
 	return h1.Sum64(), h2.Sum64()
 }
 
+func (bf *BloomFilter) set(h uint64, value bool) {
+	bf.values[h%bf.capacity] = value
+}
+
+func (bf *BloomFilter) get(h uint64) bool {
+	return bf.values[h%bf.capacity]
+}
+
 func New(capacity int) *BloomFilter {
 	return &BloomFilter{make(map[uint64]bool, capacity), uint64(capacity)}
 }
 
 func (bf *BloomFilter) Add(s string) {
 	h1, h2 := str_to_fnv(s)
-	bf.values[h1%bf.capacity] = true
-	bf.values[h2%bf.capacity] = true
+	bf.set(h1, true)
+	bf.set(h2, true)
 }
 
 func (bf *BloomFilter) IsPresent(s string) bool {
 	h1, h2 := str_to_fnv(s)
 
-	return bf.values[h1%bf.capacity] && bf.values[h2%bf.capacity]
+	return bf.get(h1) && bf.get(h2)
 }
